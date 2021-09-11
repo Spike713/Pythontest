7dc4e0d7a5c6332b5a5f8d68e7fc62f3c8b61b29
Spike713 <61779747+Spike713@users.noreply.github.com>
2021-09-11 19:01:44 +0800

    Update generate_config.py

--- training/generate_config.py
+++ training/generate_config.py
@@ -22,23 +22,23 @@ def open_workbook(excel_path:str):
 	ws = wb.get_sheet_by_name(sheetnames[0])
 	output = ""
 	for i in range(1, ws.max_row):
-		temp_str = "    [" + str(i) + "] = {\n"
+		temp_str = "[" + str(i) + "] = {\n"
 		v_list = [j.value for j in ws[i+1] if (j.value)]
 		item_type_list = v_list[::2]
 		item_count_list = v_list[1::2]
 		s = ""
 		for i in range(len(item_type_list)):
 			s = s + make_one_reward(i+1, item_type_list[i], item_count_list[i]) + '\n'
-		s = temp_str + s + "    },\n"
+		s = temp_str + s + "},\n"
 		output = output + s
 	output = "{\n" + output + "}"
 	return output
 
 
 def make_one_reward(row, item_type, item_count):
-	s = "       [{}] = {{reward_type = REWARD_TYPE_ITEM, item_type = {}, item_count = {},}},".format(row, item_type, item_count)
+	s = "[{}] = {{reward_type = REWARD_TYPE_ITEM, item_type = {}, item_count = {},}},".format(row, item_type, item_count)
 	return s
 
 
-s = open_workbook('/Users/kagami/git/python-practice-for-game-tester/res/快速生成奖励配置/配置模板.xlsx')
+s = open_workbook('/Users/desktop/快速生成奖励配置/配置模板.xlsx')
 print(s)
